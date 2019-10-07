8afe544e5c84370ca20ebe85b9c756c16b09c9ec
Eddie Huynh <ehuy0003@student.monash.edu>
2019-10-07 23:41:49 +1100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ActorComponent } from './actor/actor.component';
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from "./app.component";
+import { ActorComponent } from "./actor/actor.component";
+import { DatabaseService } from "./database.service";
+import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
-import { DatabaseService } from './database.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ActorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, ActorComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [DatabaseService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
