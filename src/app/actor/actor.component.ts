799faf037bed9938f76a87240c8a02c557353d41
Eddie Huynh <ehuy0003@student.monash.edu>
2019-10-07 18:38:38 +1100

    setting up

--- src/app/actor/actor.component.ts
+++ src/app/actor/actor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-actor',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActorComponent implements OnInit {
 
-  constructor() { }
+  // Component has access to the DatabaseService 
+  // Bring me this DatabaseService and reference "dbService" variable, each time you want to call this service use "this.dbService"
+  constructor( private dbService:DatabaseService ) { }
 
+  // event callback
   ngOnInit() {
   }
 
