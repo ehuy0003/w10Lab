ee09f1167aac7b6472664c0ee9404068686b970b
Eddie Huynh <ehuy0003@student.monash.edu>
2019-10-07 18:51:36 +1100

    as

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
+// Service depends on HTTP
 export class DatabaseService {
 
-  constructor() { }
+  constructor( private http:HttpClient ) { }
+
+  getActors(){
+    return this.http.get('/actors');
+  }
+
 }
