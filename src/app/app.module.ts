6b18f76cf5087e277d15a7f02da6e14fdfbd138d
Eddie Huynh <ehuy0003@student.monash.edu>
2019-10-07 18:17:05 +1100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActorComponent } from './actor/actor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActorComponent
   ],
   imports: [
     BrowserModule,
